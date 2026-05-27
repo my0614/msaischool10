5c507668a48beb31895f173691d8f8fb91b4c26b
my0614 <aqi222@naver.com>
2026-05-27 12:20:18 +0900

    Image open ai 구현

---
+++ openai-api/20260527_openai.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import base64
+from PIL import Image
+import io
+
+url = "https://10ai000-openai-sw.openai.azure.com/openai/deployments/gpt-image-2/images/generations?api-version=2024-02-01"
+
+payload = json.dumps({
+  "prompt": "피카소가 그린 형식의 바다 풍경을 그려줘",
+  "size": "1024x1024",
+  "quality": "low",
+  "output_compression": 100,
+  "output_format": "png",
+  "n": 1
+})
+headers = {
+  'Content-Type': 'application/json',
+  'api-key': 'os.getenv("IMAGE_API_KEY")'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+  # JSON으로 파싱 먼저
+result = response.json()
+
+b64_json = result["data"][0]["b64_json"]
+
+# b64_json → bytes → PIL Image
+image_bytes = base64.b64decode(b64_json)
+image = Image.open(io.BytesIO(image_bytes))
+
+# 파일로 저장
+image.save("output.png")
