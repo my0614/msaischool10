9420a2c302c8ddf074826680821bfb1cdcb9b4e9
my0614 <aqi222@naver.com>
2026-06-09 16:24:01 +0900

    gradio 이미지 추가 및 bounding box 추가

--- vision/20260609.py
+++ vision/20260609.py
@@ -2,6 +2,8 @@ import os
 import gradio as gr
 import requests
 from dotenv import load_dotenv
+from PIL import Image, ImageDraw
+from io import BytesIO
 
 load_dotenv()
 
@@ -30,7 +32,43 @@ def request_vision(image_url):
 
     return response_json
 
+def draw_image(image_url, data):
+    response = requests.get(image_url)
+    image = Image.open(BytesIO(response.content))
+    draw = ImageDraw.Draw(image)
 
-print(request_vision(
-    "https://images.unsplash.com/photo-1773332598289-ed0444ad1d6f"
-))
+    draw.rectangle([(100, 100), (200, 400)], outline="red", width=2)
+
+    block_list = data.get('objectsResult').get('values')
+    for block_box in block_list:
+        box = block_box.get('boundingBox')
+        name = block_box.get('tags')[0]['name']
+        conf = block_box.get('tags')[0]['confidence']
+        draw.rectangle([(box['x'], box['y']), (box['x']+box['w'], box['y']+box['h'])], outline="red", width=2)
+        draw.text((box['x'], box['y']), "{}{:.2f}%".format(name, conf*100))
+        
+    return image
+
+
+with gr.Blocks() as demo:
+
+    def click_send(image_url):
+        response_json = request_vision(image_url)
+        result_image = draw_image(image_url, response_json)
+        return response_json, result_image
+
+    image_url_text = gr.Textbox(
+        label="이미지 경로",
+        value="https://images.unsplash.com/photo-1773332598289-ed0444ad1d6f",
+    )
+    send_button = gr.Button("전송")
+
+    with gr.Row():
+        output_image = gr.Image(label="결과 이미지", interactive=False, type="pil")
+        output_json = gr.JSON(label="결과 JSON")
+
+    send_button.click(
+        click_send, inputs=[image_url_text], outputs=[output_json, output_image]
+    )
+
+demo.launch()
