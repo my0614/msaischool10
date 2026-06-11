1a5703c282e01c216f2375cfa401c2f55b975aa6
my0614 <aqi222@naver.com>
2026-06-11 16:31:45 +0900

    gradio 기능 구현

--- vision/20260611_opencv.py
+++ vision/20260611_opencv.py
@@ -4,7 +4,9 @@ import cv2
 from PIL import Image
 
 
-def detect_object(image_array):
+def detect_object(image_array, is_bgr=True):
+    image_array = image_array.copy()  # read-only 배열 → 쓰기 가능 복사본
+
     cascade_file_path = (
         cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
     )
@@ -20,7 +22,8 @@ def detect_object(image_array):
     for x, y, w, h in bounding_boxes:
         cv2.rectangle(image_array, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
+    if is_bgr:
+        image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
 
     return image_array
 
@@ -35,3 +38,25 @@ output_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "result_2
 result_bgr = cv2.cvtColor(result, cv2.COLOR_RGB2BGR)
 cv2.imwrite(output_path, result_bgr)
 print(f"결과 이미지 저장 완료: {output_path}")
+
+with gr.Blocks() as demo:
+
+    def stream_webcam(image_array):
+        result_image = detect_object(image_array=image_array, is_bgr=False)
+        return result_image
+
+    gr.Markdown("객체 감지 서비스")
+
+    with gr.Row():
+        stream_image = gr.Image(
+            label="웹캠",
+            sources=["webcam"],
+            webcam_options=gr.WebcamOptions(mirror=False),
+        )
+        result_image = gr.Image(label="감지 결과", interactive=False)
+
+    stream_image.stream(
+        stream_webcam, inputs=[stream_image], outputs=[result_image]
+    )
+
+demo.launch()
